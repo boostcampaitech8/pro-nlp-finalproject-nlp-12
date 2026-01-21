273da116699beb13275a93cdd7b687d367918b4d
202310957 <snu11025@naver.com>
2026-01-21 22:13:46 +0900

    chore: FE 초기 환경 업로드

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+import ShortFormCard from "@/components/feed/ShortFormCard";
+import TimelineSection from "@/components/feed/TimelineSection";
+
+export default function Main() {
+    return (
+        <div>
+            Home Page
+        </div>
+    )
+}
---
+++ frontend/src/components/feed/TimelineSection.tsx
@@ -0,0 +1,7 @@
+export default function TimelineSection() {
+    return (
+        <div>
+            TimelineSection
+        </div>
+    )
+}
