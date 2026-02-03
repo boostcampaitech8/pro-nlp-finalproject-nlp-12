9b6c3fa81cb7e13a493293881915d17d3e914d21
202310957 <snu11025@naver.com>
2026-02-03 12:49:17 +0900

    refactor: app, router 구조 일부 수정

---
+++ backend/src/api/search.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, Depends, Request
+from src.service.paper_service import PaperService
+from src.schemas.search import SearchResponse
+from typing import List
+
+router = APIRouter(
+    prefix="/api/search",
+    tags=["Search"]
+)
+
+# 서비스 인스턴스를 관리하는 함수(의존성 주입용)
+def get_paper_service(request: Request) -> PaperService:
+    return request.app.state.paper_service
+
+@router.get("/", response_model=List[SearchResponse])
+async def read_papers(
+    query: str,
+    service: PaperService = Depends(get_paper_service)    
+):
+    """
+    논문 검색 결과를 반환합니다.
+    """
+    return await service.hybrid_search(query)
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,11 +1,33 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from src.api.user import router as user_router
-from src.api.paper import router as paper_router
+from contextlib import asynccontextmanager
+from src.api import search
+from src.entity.base import init_db
+from src.service.paper_service import PaperService
+from src.service.vector_service import VectorService
+from src.repository.paper_repository import PaperRepository
+import uvicorn
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # DB 초기화
+    init_db()
+
+    # 무거운 작업 처리
+    all_papers = PaperRepository.get_papers_as_documents()
+
+    vector_service = VectorService()
+    vectorstore = vector_service.initialize_index(all_papers)
+
+    # app.state에 서비스 인스턴스 저장(어디서든 꺼내 쓸 수 있음)
+    app.state.paper_service = PaperService(vectorstore, all_papers)
+
+    yield
 
 app = FastAPI(
     title="논문 숏폼 서비스",
-    description="논문 요약 및 숏폼 추천 API 서버"
+    description="논문 요약 및 숏폼 추천 API 서버",
+    lifespan=lifespan
 )
 
 app.add_middleware(
@@ -18,9 +40,11 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-app.include_router(user_router)
-app.include_router(paper_router)
+app.include_router(search.router)
 
 @app.get("/")
 def read_root():
     return {"message": "논문 숏폼 서비스입니다."}
+
+if __name__ == "__main__":
+    uvicorn.run("src.main:app", host="127.0.0.1", port=8080, reload=True)
