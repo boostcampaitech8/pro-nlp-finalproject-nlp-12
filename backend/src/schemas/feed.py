e7584a3abd198ef5c51b8c34e55a32d9e6a689f2
202310957 <snu11025@naver.com>
2026-02-06 10:14:11 +0900

    refactor: FeedItem 수정

--- backend/src/schemas/feed.py
+++ backend/src/schemas/feed.py
@@ -18,19 +18,6 @@ class FeedItem(BaseModel):
 
     score: float
 
-    """
-    논문 추천 결과를 관리하는 스키마입니다.
-    """
-    paper_id: int
-    arxiv_id: str
-    title: str
-    pdf_url: HttpUrl
-    abs_url: HttpUrl
-    published_date: date
-    summary: str
-    is_liked: bool
-    is_bookmarked: bool
-
 class FeedResponse(BaseModel):
     user_id: str
 
